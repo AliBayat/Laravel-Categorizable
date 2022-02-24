f8339ea8aa572ef0367bad5f09ae77e50ba6521e
Parham <Parham.parnian@gmail.com>
2022-02-24 13:08:17 +0330

    Table names config added

--- config/laravel-categorizable.php
+++ config/laravel-categorizable.php
@@ -26,4 +26,28 @@ return [
 
     ],
 
+    'table_names' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Package's tables
+        |--------------------------------------------------------------------------
+        */
+
+        /*
+         * When using the "Categorizable" trait from this package, we need to know which
+         * table should be used to retrieve your roles. We have chosen a basic
+         * default value but you may easily change it to any table you like.
+         */
+        'main_table'  => 'categories',
+
+        /*
+         * When using the "Categorizable" trait from this package, we need to know which
+         * table should be used to retrieve your roles. We have chosen a basic
+         * default value but you may easily change it to any table you like.
+         */
+        'morph_table' => 'categories_models',
+
+    ],
+
 ];
