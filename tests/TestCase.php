8e1e6d64af6faf47d5efbd948f764f6819d7c716
AliBayat <ali.bayat@live.com>
2022-12-23 15:24:39 +0330

    prepare test environment

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,31 +2,56 @@
 
 namespace AliBayat\LaravelCategorizable\Tests;
 
+use AliBayat\LaravelCategorizable\CategorizableServiceProvider;
+use Illuminate\Config\Repository;
+use Illuminate\Contracts\Foundation\Application as ApplicationContract;
 use Illuminate\Database\Schema\Blueprint;
-use AliBayat\LaravelCategorizable\CategorizableServiceProvide;
+use Illuminate\Foundation\{Application, Testing\DatabaseMigrations};
+use Kalnoy\Nestedset\NestedSet;
 use Orchestra\Testbench\TestCase as Orchestra;
 
-class TestCase extends Orchestra
+abstract class TestCase extends Orchestra
 {
+	use DatabaseMigrations;
+	
+	/**
+	 * @var string|ApplicationContract|Repository
+	 */
+	protected string|ApplicationContract|Repository $mainTable;
+	
+	/**
+	 * @var string|ApplicationContract|Repository
+	 */
+	protected string|ApplicationContract|Repository $morphTable;
+	
+	/**
+	 * @return void
+	 */
     public function setUp(): void
     {
         parent::setUp();
-
+		$this->mainTable = config('laravel-categorizable.table_names.main_table');
+		$this->morphTable = config('laravel-categorizable.table_names.morph_table');
         $this->setUpDatabase();
     }
-
-    protected function getPackageProviders($app)
+	
+	/**
+	 * @param Application $app
+	 * @return array
+	 */
+    protected function getPackageProviders($app): array
     {
         return [
-            CategorizableServiceProvide::class,
+            CategorizableServiceProvider::class,
         ];
     }
-
-
-    /**
-     * @param \Illuminate\Foundation\Application $app
-     */
-    protected function getEnvironmentSetUp($app)
+	
+	
+	/**
+	 * @param Application $app
+	 * @return void
+	 */
+    protected function getEnvironmentSetUp($app): void
     {
         $app['config']->set('database.default', 'sqlite');
         $app['config']->set('database.connections.sqlite', [
@@ -35,27 +60,39 @@ class TestCase extends Orchestra
             'prefix' => '',
         ]);
     }
-
-    protected function setUpDatabase()
+	
+	/**
+	 * @return void
+	 */
+    protected function setUpDatabase(): void
     {
-        $this->app['db']->connection()->getSchemaBuilder()->create(config('laravel-categorizable.table_names.main_table'), function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('custom_column_sort');
-            $table->integer('order_column');
+        $this->app['db']->connection()->getSchemaBuilder()->create($this->mainTable, static function (Blueprint $table) {
+	        $table->increments('id');
+	        $table->string('name');
+	        $table->string('slug');
+	        $table->string('type')->default('default');
+	        NestedSet::columns($table);
+	        $table->timestamps();
         });
-
-        collect(range(1, 20))->each(function (int $i) {
-            Dummy::create([
-                'name' => $i,
-                'custom_column_sort' => rand(),
-            ]);
+		
+        $this->app['db']->connection()->getSchemaBuilder()->create($this->morphTable, static function (Blueprint $table) {
+	        $table->integer('category_id');
+	        $table->morphs('model');
+        });
+		
+        $this->app['db']->connection()->getSchemaBuilder()->create('posts', static function (Blueprint $table) {
+	        $table->increments('id');
+	        $table->string('title');
+	        $table->string('body');
         });
     }
-
-    protected function setUpSoftDeletes()
+	
+	/**
+	 * @return void
+	 */
+    protected function setUpSoftDeletes(): void
     {
-        $this->app['db']->connection()->getSchemaBuilder()->table(config('laravel-categorizable.table_names.main_table'), function (Blueprint $table) {
+        $this->app['db']->connection()->getSchemaBuilder()->table($this->mainTable, function (Blueprint $table) {
             $table->softDeletes();
         });
     }
